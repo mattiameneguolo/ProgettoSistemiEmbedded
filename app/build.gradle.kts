3c24682c0bc4a361a7e05250738c287753889778
mattiameneguolo <mattiameneguolo@gmail.com>
2026-05-18 12:12:50 +0200

    Fix warning segnalati da lint

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,8 +46,6 @@ android {
 }
 
 dependencies {
-    val nav_version = "2.9.7"
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -57,7 +55,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.constraintlayout.compose)
-    implementation("androidx.navigation:navigation-compose:${nav_version}")
+    implementation(libs.androidx.navigation.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
