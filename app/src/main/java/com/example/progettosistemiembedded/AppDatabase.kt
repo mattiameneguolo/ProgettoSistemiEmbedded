a6b7d6e3680a497635c8af901aaeedfd1cf9b99d
mattiameneguolo <mattiameneguolo@gmail.com>
2026-05-29 00:23:23 +0200

    Aggiunti commenti a AppDatabase.kt

--- app/src/main/java/com/example/progettosistemiembedded/AppDatabase.kt
+++ app/src/main/java/com/example/progettosistemiembedded/AppDatabase.kt
@@ -5,7 +5,19 @@ import androidx.room.RoomDatabase
 import com.example.progettosistemiembedded.database.game.Game
 import com.example.progettosistemiembedded.database.game.GameDao
 
+/**
+ * Classe che rappresenta il database dell'applicazione.
+ * La classe utilizza Room per gestire la persistenza dei dati.
+ *
+ * Il database contiene solamente un'entità: Game
+ */
 @Database(entities = [Game::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
+
+    /**
+     * Dao utilizzato per accedere ai dati dell'entità Game.
+     *
+     * @return Istanza di GameDao
+     */
     abstract fun gameDao(): GameDao
 }
