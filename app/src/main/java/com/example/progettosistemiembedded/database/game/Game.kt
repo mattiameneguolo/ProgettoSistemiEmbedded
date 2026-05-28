7a301dcbfe0c163228880d09889eb5e02b77f682
mattiameneguolo <mattiameneguolo@gmail.com>
2026-05-29 00:27:40 +0200

    Aggiunti commenti a Game.kt e GameDao.kt

--- app/src/main/java/com/example/progettosistemiembedded/database/game/Game.kt
+++ app/src/main/java/com/example/progettosistemiembedded/database/game/Game.kt
@@ -3,6 +3,15 @@ package com.example.progettosistemiembedded.database.game
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
+/**
+ * Entità che rappresenta la struttura della tabella "games" nel database.
+ *
+ * Ogni istanza di questa classe rappresenta un'entità nel database,
+ *
+ * @property id = identificativo univoco nella partita, generato automaticamente
+ * @property sequence = sequenza di caratteri che rappresenta la partita
+ * @property errorIndex = indice del carattere di errore nella sequenza (-1 se non ci sono errori)
+ */
 @Entity(tableName = "games")
 data class Game (
     @PrimaryKey(autoGenerate = true)
--- app/src/main/java/com/example/progettosistemiembedded/database/game/GameDao.kt
+++ app/src/main/java/com/example/progettosistemiembedded/database/game/GameDao.kt
@@ -4,14 +4,34 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
 
+/**
+ * Dao (Data Access Object) per l'interazione con la tabella "games" nel database.
+ */
 @Dao
 interface GameDao {
+
+    /**
+     * Recupera tutte le partite salvate nel database.
+     *
+     * @return lista di oggetti Game salvati nella tabella "games"
+     */
     @Query("SELECT * FROM games")
     suspend fun getAllGames(): List<Game>
 
+    /**
+     * Recupera una singola partita dal database in base all'ID.
+     *
+     * @param gameId = identificativo della partita da cercare
+     * @return oggetto Game corrispondente all'ID fornito
+     */
     @Query("SELECT * FROM games WHERE id = :gameId")
     suspend fun getGameById(gameId: Int): Game
 
+    /**
+     * Inserisce una nuova partita nel database.
+     *
+     * @param game = oggetto Game da inserire nel database
+     */
     @Insert
     suspend fun insertGame(game: Game)
 }
